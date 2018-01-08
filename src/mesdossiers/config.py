2a60523c56a9adb8a16b22d047e7ae7d9f122458
Adam Scott <ascott.ca@gmail.com>
2018-01-07 19:25:38 -0500

    Add SECRET_KEY to default config.

--- src/mesdossiers/config.py
+++ src/mesdossiers/config.py
@@ -1,8 +1,12 @@
+import os
+
+
 class Config:
     DEBUG = False
     TESTING = False
     SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key')
 
 
 class ProductionConfig(Config):

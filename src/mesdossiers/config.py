fbc3cb3163f5547c36e75e908da86cabae30143a
Adam Scott <ascott.ca@gmail.com>
2018-01-08 08:43:09 -0500

    Change default database from SQLite (from memory) to POSTGRES.

--- src/mesdossiers/config.py
+++ src/mesdossiers/config.py
@@ -4,7 +4,12 @@ import os
 class Config:
     DEBUG = False
     TESTING = False
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://{user}:{pw}@{url}/{db}'.format(
+        user=os.getenv('POSTGRES_USER', 'postgres'),
+        pw=os.getenv('POSTGRES_PASSWORD', ''),
+        url=os.getenv('POSTGRES_URL', 'localhost:5432'),
+        db=os.getenv('POSTGRES_DB', 'mes-dossiers')
+    )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key')
 
